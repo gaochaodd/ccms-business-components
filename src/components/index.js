704c46024ed3b879da3cf2c045c5b0cf37540d18
hjzheng <zhenghaoju700@163.com>
2018-12-25 10:16:32 +0800

    短信编辑器

--- src/components/index.js
+++ src/components/index.js
@@ -8,12 +8,16 @@ import angular from 'angular';
 
 import areaSelector from './area-selector';
 import customerProfileBoard from './customer-profile-board';
+import smsEditor from './sms-editor';
+import smsPreview from './sms-preview';
 
 
 export default angular
 	.module('ccms.components.business', [
 		areaSelector,
-		customerProfileBoard
+		customerProfileBoard,
+		smsEditor,
+		smsPreview
 	])
 	.name;
 
